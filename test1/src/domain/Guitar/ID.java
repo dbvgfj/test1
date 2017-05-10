80662ca0d67e23d34b7096768585b066bcdb5041
dbvgfj <847636182@qq.com>
2017-05-10 09:21:43 +0800

    Revert "1"
    This reverts commit aabe20bfb46fa0d24b2f1164e5f976d9200f0159.

--- test1/src/domain/Guitar/ID.java
+++
@@ -1,6 +0,0 @@
-package domain.Guitar;
-
-public interface  ID {
-	public Guitar findserialNumber(double price,double price2,String bilder,String model,String type,String backwood,String topwood) ;	
-	public Guitar findguitar(int serialNumber) ;	
-}
